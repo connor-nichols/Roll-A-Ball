4d7b77cc80859bd17dfd771ca17e4d50dbee6555
Connor Nichols <32244381+connor-nichols@users.noreply.github.com>
2023-01-26 20:32:51 -0600

    finish tutorial, first build

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,19 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
 
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
+
+    public float collectables = 0;
+
+
+
+    private AudioSource _audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        _audioSource = GetComponent<AudioSource>();
+        if(_audioSource == null)
+        {
+            Debug.LogError("AudioSource not found!");
+        }
+
+        count = 0;
+        SetCountText();
+        winTextObject.gameObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -23,6 +44,15 @@ public class PlayerController : MonoBehaviour
 
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if(count >= collectables)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 
     void FixedUpdate()
@@ -36,7 +66,10 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("PickUp"))
         {
+            _audioSource.Play();
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
         }
         
 
